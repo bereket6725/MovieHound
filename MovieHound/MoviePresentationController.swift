648d583b6b91fd3a7e2fa0657f31a0a5fcbc7b4e
Bereket <bereket6725@gmail.com>
2017-02-13 18:42:10 -0500

    making presentationController

---
+++ MovieHound/MoviePresentationController.swift
@@ -0,0 +1,13 @@
+//
+//  MoviePresentationController.swift
+//  MovieHound
+//
+//  Created by Bereket Ghebremedhin on 2/13/17.
+//  Copyright © 2017 Bereket Ghebremedhin. All rights reserved.
+//
+
+import UIKit
+
+class MoviePresentationController: UIPresentationController {
+
+}
