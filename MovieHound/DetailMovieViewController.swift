a06b2916e34a4b73a3ee6e74ddf78e8b08226dd3
Bereket <bereket6725@gmail.com>
2017-02-13 20:23:31 -0500

    setup DetailMovieViewController with its Movie info, size and dismissal

--- MovieHound/DetailMovieViewController.swift
+++ MovieHound/DetailMovieViewController.swift
@@ -10,10 +10,36 @@ import UIKit
 
 class DetailMovieViewController: UIViewController {
 
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+
+    var movieItem:MovieModel? {
+        didSet{
+            configureView()
+        }
+    }
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.view.bounds.size = CGSize(width: UIScreen.main.bounds.size.width - 20, height: 350)
+        self.view.layer.cornerRadius = 5
+
+    }
+    func configureView(){
+        if let movie = self.movieItem{
+            self.titleLabel.text = movie.title
+            self.descriptionTextView.text = movie.description
+        }
+    }
+
+    @IBAction func dismissButtonTapped(_ sender: Any) {
+        presentingViewController?.dismiss(animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +47,6 @@ class DetailMovieViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
