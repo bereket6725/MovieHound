ee3edfdfab1242435c493fb9a4bb0a433880243d
Bereket Ghebremedhin <bereket6725@gmail.com>
2017-03-21 16:54:25 -0400

    updated Readme and added screenshots

--- MovieHound/MoviePresentationController.swift
+++ MovieHound/MoviePresentationController.swift
@@ -9,37 +9,33 @@
 import UIKit
 
 class MoviePresentationController: UIPresentationController, UIAdaptivePresentationControllerDelegate{
-
+    
     var dimmingView = UIView()
-
+    
     override var shouldPresentInFullscreen: Bool {
         return true
     }
-
+    
     override init(presentedViewController: UIViewController, presenting presentingViewController: UIViewController?) {
         super.init(presentedViewController: presentedViewController, presenting: presentingViewController)
-
         dimmingView.backgroundColor = UIColor(white: 0, alpha: 0.8)
         dimmingView.alpha = 0
-
     }
-
+    
     override func presentationTransitionWillBegin() {
         dimmingView.frame = self.containerView!.bounds
         dimmingView.alpha = 0
         containerView?.insertSubview(dimmingView, at: 0)
-
-
         if let coordinator = presentedViewController.transitionCoordinator {
             coordinator.animate(alongsideTransition: { (context:UIViewControllerTransitionCoordinatorContext) in
                 self.dimmingView.alpha = 1
             }, completion: nil)
-        }else{
+        } else {
             dimmingView.alpha = 1
         }
-
+        
     }
-
+    
     override func dismissalTransitionWillBegin() {
         if let coordinator = presentedViewController.transitionCoordinator {
             coordinator.animate(alongsideTransition: { (context:UIViewControllerTransitionCoordinatorContext) in
@@ -49,15 +45,15 @@ class MoviePresentationController: UIPresentationController, UIAdaptivePresentat
             dimmingView.alpha = 0
         }
     }
-
+    
     override func containerViewWillLayoutSubviews() {
         if let containerBounds = containerView?.bounds {
             dimmingView.frame = containerBounds
             presentedView?.frame = containerBounds
         }
     }
-
-
+    
+    
     func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
         return .overFullScreen
     }
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MovieHound
-An app to let you know about upcoming movies and where to find them
+An app to let you know about upcoming movies, their reviews and song credits. Work in Progress :) 
+
+![Screenshot](ScreenShots/NowPlaying.png)
+![Screenshot](ScreenShots/Detail.png)
+
+1. Accesses the [Movie Databases API](https://www.themoviedb.org/documentation/api) to display now playing movies.
+2. UICollectionView Cells that hold movie thumbnails and titles can be tapped to reveal: A. a description and  B: a link to reviews (In progress). 
+3. Retrieves songs from movies when cells are tapped [(In progress)](https://github.com/bereket6725/MovieHound/issues/2)
+
+
+Going to use a [library](https://github.com/rs/SDWebImage) to Cache the poster images instead of havign a custom one 
+
+
+# License 
+Code is released under MIT 
